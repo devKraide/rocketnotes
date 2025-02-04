08b878f436a2235e35d00ec7083da075598baab3
Nicolas <nicolas.kraide@gamil.com>
2025-02-04 20:25:23 -0300

    fragment

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,6 +1,11 @@
+import { Fragment } from "react";
+// fragment is a wrapper that allows you to return multiple elements without adding a div. The con of using a fragment is that it doesn't support keys or attributes => styled-components. Different from a div.
 export function Details() {
   
   return (
+    <Fragment>
   <h1>Hello World!</h1>  
+  <span>Made by Nicolas Kraide A.K.A the best developer of his house</span>
+  </Fragment>
   )
 }
