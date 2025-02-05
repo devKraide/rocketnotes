adf8e8abfed79c600624f714508b564deb5fd197
Nicolas <nicolas.kraide@gamil.com>
2025-02-05 10:02:57 -0300

    css in js

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,15 +1,15 @@
 //import { Fragment } from "react";
 // fragment is a wrapper that allows you to return multiple elements without adding a div. The con of using a fragment is that it doesn't support keys or attributes => styled-components. Different from a div.
 
-import "./styles.css";
+import { Container } from "./styles.js";
 
 export function Details() {
 
   return (
-    <div>
+    <Container>
 
       <h1>Hello World!</h1>
-      
-    </div>
+
+    </Container>
   )
 }
