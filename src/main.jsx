4360886d86d87e1be4cd899ad73fbc327943b1fe
Nicolas <nicolas.kraide@gamil.com>
2025-02-05 10:16:11 -0300

    theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {ThemeProvider}  from 
+'styled-components';
+
+import { theme } from './styles/theme';
+
 import { Details } from './pages/Details';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode >
+    <ThemeProvider theme = {theme}>
     <Details />
-  </StrictMode>,
+    </ThemeProvider>
+  </React.StrictMode>,
 )
