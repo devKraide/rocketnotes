5c21df92f51a0a825d1f66d6c996f0cbd445ce4e
Nicolas <nicolas.kraide@gamil.com>
2025-02-06 08:30:27 -0300

    update props

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,14 +1,17 @@
 import { Container } from "./styles";
 import PropTypes from 'prop-types';
 
-export function Button(props) {
+//proptypes is a library that allows you to define the type of props that a component should receive. It is a good practice to use it to avoid bugs in the future.
+
+Button.propTypes = {
+  title: PropTypes.string.isRequired,
+}
+
+export function Button({title}) {
   return (
     <Container type="button">
-      {props.title}      
+      {title}      
     </Container>
   )
 }
 
-Button.propTypes = {
-  title: PropTypes.string.isRequired,
-};
