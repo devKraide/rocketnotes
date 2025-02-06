dbf5c31fbb3d7097bf325c621507855a06f5bbc7
Nicolas <nicolas.kraide@gamil.com>
2025-02-06 07:52:49 -0300

    component - button

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -2,6 +2,7 @@
 // fragment is a wrapper that allows you to return multiple elements without adding a div. The con of using a fragment is that it doesn't support keys or attributes => styled-components. Different from a div.
 
 import { Container } from "./styles.js";
+import { Button } from "../../components/Button";
 
 export function Details() {
 
@@ -11,6 +12,7 @@ export function Details() {
       <h1>Hello World!</h1>
       <p>made by Nicolas Kraide, A.K.A best developer of his house</p>
 
+    <Button/>
     </Container>
   )
 }
