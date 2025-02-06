027ee4d1a54c83a5be10fb12429dc7b71645a4fa
Nicolas <nicolas.kraide@gamil.com>
2025-02-06 09:01:31 -0300

    updating properties

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -5,13 +5,19 @@ import PropTypes from 'prop-types';
 
 Button.propTypes = {
   title: PropTypes.string.isRequired,
+  loading: PropTypes.bool
 }
 
-export function Button({title}) {
+export function Button({title, loading = false, ...rest}) {
   return (
-    <Container type="button">
-      {title}      
+    <Container 
+    type="button"
+    disabled={loading}
+    {...rest}
+    >
+      {loading ? 'loading...' : title}      
     </Container>
   )
 }
 
+// The button component receives two props, title and loading. The title is a string and is required, while loading is a boolean and is not required. If loading is true, the button will be disabled. If loading is false, the button will be enabled and will display the title.
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -12,7 +12,7 @@ export function Details() {
       <h1>Hello World!</h1>
       <p>made by Nicolas Kraide, A.K.A best developer of his house</p>
 
-    <Button title="join"/>
+    <Button title="join" loading/>
     <Button title="nicolas"/>
     <Button title="cadaster"/>
 
