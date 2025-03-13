408285ee238800a9e4e8d355710088ebe8b538cc
Nicolas <nicolas.kraide@gamil.com>
2025-03-13 18:33:26 -0300

    creating 'New' page

---
+++ src/pages/New/index.jsx
@@ -0,0 +1,11 @@
+import { NewContainer } from "./style";
+
+import {Header} from "../../components/Header";
+
+export function New () {
+  return (
+    <NewContainer>
+      <Header/>
+    </NewContainer>
+  )
+}
---
+++ src/pages/New/style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const NewContainer = styled.div `
+  width: 100%;
+  height: 100vh;
+
+  display: grid;
+  grid-template-rows: 105px auto;
+  grid-template-areas:
+  "header" 
+  "content";
+`;
