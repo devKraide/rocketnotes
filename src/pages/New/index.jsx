b8a2ae00c281e6eb517b91c91b5a80c3a6f5a050
Nicolas <nicolas.kraide@gamil.com>
2025-03-13 18:40:29 -0300

    feat: implement Header in New page

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,11 +1,24 @@
-import { NewContainer } from "./style";
-
 import {Header} from "../../components/Header";
+import {Input} from "../../components/Input";
+
+import { NewContainer, Form } from "./style";
 
 export function New () {
   return (
     <NewContainer>
       <Header/>
+
+      <main>
+        <Form>
+          <header>
+            <h1>Criar nota</h1>
+            <a href="/">voltar</a>
+          </header>
+          <Input/>
+        </Form>
+      </main>
+
     </NewContainer>
   )
+  
 }
