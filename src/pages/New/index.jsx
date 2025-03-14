ccec8ab11a1ac069188972a824498b3274b5d328
Nicolas <nicolas.kraide@gamil.com>
2025-03-14 09:25:48 -0300

    feat: enhance New page layout with additional sections and button

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,15 +1,16 @@
-import {Header} from "../../components/Header";
-import {Input} from "../../components/Input";
-import {TextArea} from "../../components/TextArea";
-import {NoteItem} from "../../components/NoteItem";
-import {Section} from "../../components/Section";
+import { NoteItem } from "../../components/NoteItem";
+import { TextArea } from "../../components/TextArea";
+import { Section } from "../../components/Section";
+import { Header } from "../../components/Header";
+import { Button } from "../../components/Button";
+import { Input } from "../../components/Input";
 
 import { NewContainer, Form } from "./style";
 
-export function New () {
+export function New() {
   return (
     <NewContainer>
-      <Header/>
+      <Header />
 
       <main>
         <Form>
@@ -19,18 +20,29 @@ export function New () {
             <a href="/">voltar</a>
           </header>
 
-          <Input placeholder= 'Título'/>
-          <TextArea placeholder= 'Observações'/>
+          <Input placeholder='Título' />
+          <TextArea placeholder='Observações' />
 
-          <Section title = "Links úteis">	
-          <NoteItem value="https://apple.com"/>  
-          <NoteItem isNew placeholder='Novo link'/>  
+          <Section title="Links úteis">
+            <NoteItem value="https://apple.com" />
+            <NoteItem isNew placeholder='Novo link' />
           </Section>
 
+
+          <Section title="Marcadores">
+            <div className="tags">
+
+              <NoteItem value="React" />
+              <NoteItem isNew placeholder='Novo marcador' />
+            </div>
+
+          </Section>
+
+          <Button title="Salvar"/>
         </Form>
       </main>
 
     </NewContainer>
   )
-  
+
 }
