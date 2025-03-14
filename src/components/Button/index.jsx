408df6dfaf2af9ad452e4d4667a86e016fb914ed
Nicolas <nicolas.kraide@gamil.com>
2025-03-14 10:14:28 -0300

    feat: implement authentication routes

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,12 +1,5 @@
-import { Container } from "./styles";
-import PropTypes from 'prop-types';
-
-//proptypes is a library that allows you to define the type of props that a component should receive. It is a good practice to use it to avoid bugs in the future.
-
-Button.propTypes = {
-  title: PropTypes.string.isRequired,
-  loading: PropTypes.bool
-}
+/* eslint-disable react/prop-types */
+import { Container } from './styles'
 
 export function Button({ title, loading = false, ...rest }) {
   return (
@@ -15,9 +8,7 @@ export function Button({ title, loading = false, ...rest }) {
       disabled={loading}
       {...rest}
     >
-      {loading ? 'loading...' : title}
-    </Container>
+      {loading ? 'Carregando...' : title}
+    </Container >
   )
 }
-
-// The button component receives two props, title and loading. The title is a string and is required, while loading is a boolean and is not required. If loading is true, the button will be disabled. If loading is false, the button will be enabled and will display the title.
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,9 +1,13 @@
-import { BrowserRouter } from 'react-router-dom';
+/* eslint-disable no-unused-vars */
+import { BrowserRouter } from "react-router-dom";
 
-import { AppRoutes } from './app.routes';
+import { AppRoutes } from "./app.routes";
+import { AuthRoutes } from "./auth.routes";
 
-export function Routes () {
-  <BrowserRouter>
-    <AppRoutes />
-  </BrowserRouter>
+export function Routes() {
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  )
 }
