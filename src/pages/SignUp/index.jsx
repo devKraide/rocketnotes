0d708c7371f092469b51303539282fda99dbaacf
Nicolas <nicolas.kraide@gamil.com>
2025-03-20 10:16:14 -0300

    feat: implement sign-up functionality with form validation

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,17 +1,30 @@
+/* eslint-disable no-unused-vars */
+import { useState } from 'react'
+
 import { FiMail, FiLock, FiUser } from "react-icons/fi";
 
-import { Link } from "react-router-dom";  
+import { Link } from "react-router-dom";
 
-import {Input} from "../../components/Input";
-import {Button} from "../../components/Button";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
 
 import { SignUpContainer, Form, Background } from "./style";
 
-export function SignUp() {
 
-  return (
+export function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function HandleSignUp() {
+    if(!name || !email || !password){
+      return alert('Preencha todos os campos!')
+    }
+  }
+
+  return (  
     <SignUpContainer>
-      <Background/>
+      <Background />
 
       <Form>
         <h1>Rocket Notes</h1>
@@ -20,26 +33,34 @@ export function SignUp() {
         <h2>Crie sua conta</h2>
 
         <Input
-          placeholder = "Nome"
-          type = "text"
+          placeholder="Nome"
+          type="text"
           icon={FiUser}
+
+          onChange={e => setName(e.target.value)}
         />
 
         <Input
-          placeholder = "E-mail"
-          type = "text"
+          placeholder="E-mail"
+          type="text"
           icon={FiMail}
+
+          onChange={e => setEmail(e.target.value)}
         />
 
         <Input
-          placeholder = "Senha"
-          type = "password"
+          placeholder="Senha"
+          type="password"
           icon={FiLock}
+
+          onChange={e => setPassword(e.target.value)}
         />
 
-        <Button title="Cadastrar"/>
+        <Button title="Cadastrar" onClick={HandleSignUp}/>
 
-        <Link to="/" >Voltar para o login</Link> 
+        <Link to="/">
+          Voltar para o login
+        </Link>
 
       </Form>
 
